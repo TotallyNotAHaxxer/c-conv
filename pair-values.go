208401588f075b20952b73202ffdd36fda122f20
RE43P3R <87203697+ArkAngeL43@users.noreply.github.com>
2022-04-18 14:39:48 -0400

    Create pair-values.go

---
+++ pair-values.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+)
+
+func printpairs(n int) {
+	for i := 1; i*i <= n; i++ {
+		if n%i == 0 {
+			fmt.Println(i, "*", n)
+		}
+	}
+}
+
+func main() {
+	printpairs(24)
+}
